030ddef3e00345919c3b292aac5b97bf125155cf
Moe Misri <mohammed.masry74@gmail.com>
2023-05-30 06:07:34 +0300

    reverse_string and palindrome

--- JavaScript/reverse_string.js
+++ JavaScript/reverse_string.js
@@ -1,27 +1,28 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  // const characters = [];
-  // const reverseString = [];
-  // for (const char of str) {
-  //   characters.push(char);
-  // }
-  // for (const char in str) {
-  //   reverseString.push(characters.pop());
-  // }
-  // return reverseString;
-
+  // 1st way
   // let reverse = "";
   // for (const char of str) {
   //   reverse = char + reverse;
   // }
   // return reverse;
 
+  // // 2nd way
+
+  // // "hello".split = ["h","e","l"....]
+
+  // const characters = str.split("");
+  // const reverseString = [];
+  // for (const char in str) {
+  //   reverseString.push(characters.pop());
+  // }
+  // return reverseString.join("");
+
+  // 3rd way
   return str.split("").reduce((rev, char) => char + rev, "");
 }
+console.log("reverse", reverse("hello"));
+// https://github.com/moemisri/problem_solving
